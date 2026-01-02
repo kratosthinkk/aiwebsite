d34874cd1e07afd07998e4b3502eddb5d9f4a6e9
dashlem <163656115+kratosthinkk@users.noreply.github.com>
2026-01-02 16:46:11 +0100

    Delete src directory

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css'; // Si tu utilises Tailwind
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
